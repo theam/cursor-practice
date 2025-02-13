fbb8ae4ecfc0488b8f423cc19a811ba3f50b82ea
adriangmweb <adriangonzalezmesa@gmail.com>
2025-02-13 11:34:43 +0000

    rename function in many files

--- src/composerUsageTwo.js
+++ src/composerUsageTwo.js
@@ -1,13 +1,13 @@
 /**
  * Also references the same function from composer.js.
  */
-const { multiplyComposerNumbers } = require('./composer');
+const { multiply } = require("./composer");
 
 function calculateVolumes(cubes) {
   // Each cube has { width, height, depth }
   // We'll multiply them to get volume
   return cubes.map(({ width, height, depth }) =>
-    multiplyComposerNumbers(multiplyComposerNumbers(width, height), depth)
+    multiply(multiply(width, height), depth)
   );
 }
 
